dc7ad317a5743f6003504857669880ca4183248c
wdidwiky <101820999+Wdidwiky@users.noreply.github.com>
2022-04-19 16:50:13 +0700

    Add project files.

---
+++ Karyawan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4324
+{
+    public class Karyawan
+    {
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+
+        public void Gaji()
+        {
+
+            Console.Write(NIK);
+            Console.Write(Nama);
+            Console.Write(GajiBulanan);
+            Console.WriteLine();
+        }
+
+        public void NaikGaji()
+        {
+            int naik = GajiBulanan + (GajiBulanan * 10 / 100);
+            Console.Write(NIK);
+            Console.Write(Nama);
+            Console.Write(naik);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ResponsiPemrograman4324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan satu = new Karyawan();
+            Karyawan dua = new Karyawan();
+
+            Console.WriteLine("No   Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("---------------------------------------");
+
+            satu.NIK = "1. 21114324";
+            satu.Nama = " Dzaky     ";
+            satu.GajiBulanan = Convert.ToInt32("    1000000");
+
+            dua.NIK = "2. 21114253";
+            dua.Nama = " Mahmud     ";
+            dua.GajiBulanan = Convert.ToInt32("   2500000");
+
+            satu.Gaji();
+            dua.Gaji();
+
+            Console.WriteLine();
+            Console.WriteLine("Asyiiiiik naik gaji 10%!!");
+            Console.WriteLine();
+            Console.WriteLine("No   Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("---------------------------------------");
+
+            satu.NaikGaji();
+            dua.NaikGaji();
+
+        }
+    }
+}
